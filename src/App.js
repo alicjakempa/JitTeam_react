7b580668971b478366224c0da8866c432c3826f7
Alicja <53865337+alicjakempa@users.noreply.github.com>
2021-02-15 18:48:25 +0100

    configuration of webpack
    and one react commponent added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                O boże, to działa!
+            </div>
+        );
+    }
+}
+export default App
